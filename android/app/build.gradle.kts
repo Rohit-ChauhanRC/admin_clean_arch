e7db38fcac4582dfa796fd55680d18b6074d9dfb
Genmak <genmak@MacBookPro.mshome.net>
2025-03-19 16:02:59 +0530

    remove ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.codebyrc.admin_clean_arch"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
